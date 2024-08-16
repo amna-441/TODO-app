c7aa9ccba93b6413996ff55ea55cfec181fda286
amna-441 <amnaakram1004@gmail.com>
2024-08-17 01:57:26 +0500

    todo app added

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color Red = Color(0xFFDA4040);
+const Color blue = Color.fromARGB(255, 72, 59, 212);
+
+const Color Black = Color.fromARGB(255, 0, 0, 0);
+const Color Grey = Color(0xFF717171);
+
+const Color white = Color(0xFFEEEFF5);
